b7886297ea690bfd84fc5399f761c2f81ac51870
sairoutine <sairoutine@gmail.com>
2017-02-03 00:00:30 +0900

    update: object base class

--- object/base.js
+++ object/base.js
@@ -5,11 +5,16 @@ var ObjectBase = function(scene) {
 	this.core = scene.core;
 
 	this.frame_count = 0;
+
+	this.x = 0;
+	this.y = 0;
 };
 
 ObjectBase.prototype.init = function(){
 	this.frame_count = 0;
 
+	this.x = 0;
+	this.y = 0;
 };
 
 ObjectBase.prototype.beforeDraw = function(){
