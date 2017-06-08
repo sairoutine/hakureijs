f3dc309d23868ef4477ef78d1463ff1b5f686587
sairoutine <sairoutine@gmail.com>
2017-06-08 16:59:41 +0900

    update: implement init method in sprite3d class

--- object/sprite3d.js
+++ object/sprite3d.js
@@ -42,6 +42,89 @@ Sprite3d.prototype.init = function(){
 	base_object.prototype.init.apply(this, arguments);
 
 	this.current_sprite_index = 0;
+
+	this._initVertices();
+	this._initCoordinates();
+	this._initIndices();
+	this._initColors();
+};
+
+Sprite3d.prototype._initVertices = function() {
+	var w = this.spriteWidth()/2;
+	var h = this.spriteHeight()/2;
+
+	this.vertices[0]  = -w;
+	this.vertices[1]  = -h;
+	this.vertices[2]  = -1.0;
+
+	this.vertices[3]  =  w;
+	this.vertices[4]  = -h;
+	this.vertices[5]  = -1.0;
+
+	this.vertices[6]  =  w;
+	this.vertices[7]  =  h;
+	this.vertices[8]  = -1.0;
+
+	this.vertices[9]  = -w;
+	this.vertices[10] =  h;
+	this.vertices[11] = -1.0;
+};
+
+Sprite3d.prototype._initCoordinates = function() {
+
+	var image = this.core.image_loader.getImage(this.spriteName());
+
+	var w = this.spriteWidth() / image.width;
+	var h = this.spriteHeight() / image.height;
+
+	var x1 = w * this.spriteIndexX();
+	var y1 = h * this.spriteIndexY();
+	var x2 = x1 + w;
+	var y2 = y1 + h;
+
+	this.coordinates[0] = x1;
+	this.coordinates[1] = y2;
+
+	this.coordinates[2] = x2;
+	this.coordinates[3] = y2;
+
+	this.coordinates[4] = x2;
+	this.coordinates[5] = y1;
+
+	this.coordinates[6] = x1;
+	this.coordinates[7] = y1;
+};
+
+Sprite3d.prototype._initIndices = function() {
+	this.indices[0] = 0;
+	this.indices[1] = 1;
+	this.indices[2] = 2;
+
+	this.indices[3] = 0;
+	this.indices[4] = 2;
+	this.indices[5] = 3;
+};
+
+Sprite3d.prototype._initColors = function() {
+	this.colors[0] = 1.0;
+	this.colors[1] = 1.0;
+	this.colors[2] = 1.0;
+	this.colors[3] = 1.0;
+
+	this.colors[4] = 1.0;
+	this.colors[5] = 1.0;
+	this.colors[6] = 1.0;
+	this.colors[7] = 1.0;
+
+	this.colors[8] = 1.0;
+	this.colors[9] = 1.0;
+	this.colors[10] = 1.0;
+	this.colors[11] = 1.0;
+
+	this.colors[12] = 1.0;
+	this.colors[13] = 1.0;
+	this.colors[14] = 1.0;
+	this.colors[15] = 1.0;
 };
 
 Sprite3d.prototype.beforeDraw = function(){
