0f76469aa7b805e2c4b678f78221059a356beba3
sairoutine <sairoutine@gmail.com>
2018-02-12 09:45:12 +0900

    implement: abolished methods in serif manager class

--- serif_manager_deprecated_notifier_base.js
+++ serif_manager_deprecated_notifier_base.js
@@ -4,36 +4,98 @@
 var SerifManager = function (option) {
 };
 
-SerifManager.prototype.init = function (script) {};
-SerifManager.prototype.setAutoStart = function (flag) {};
-SerifManager.prototype.isEnd = function () {};
-SerifManager.prototype.isStart = function () {};
-SerifManager.prototype.next = function () {};
-SerifManager.prototype._showBackground = function(script) {};
-SerifManager.prototype._showChara = function(script) {};
-SerifManager.prototype._setOption = function(script) {};
-SerifManager.prototype._printMessage = function (message) {};
-SerifManager.prototype.isWaitingNext = function () {};
-SerifManager.prototype.isEndPrinting = function () {};
-SerifManager.prototype._startPrintMessage = function () {};
-SerifManager.prototype._cancelPrintMessage = function () {};
-SerifManager.prototype.startPrintMessage = function () {};
-SerifManager.prototype.cancelPrintMessage = function () {};
-SerifManager.prototype.isBackgroundChanged = function () {};
-SerifManager.prototype.getBackgroundImageName = function () {};
-SerifManager.prototype.getImageName = function (pos) {};
-SerifManager.prototype.getChara = function (pos) {};
-SerifManager.prototype.isTalking = function (pos) {};
-SerifManager.prototype.getOption = function () {};
-SerifManager.prototype.lines = function () {};
-SerifManager.prototype.getSerifRowsCount = function () {};
-SerifManager.prototype.right_image = function () {};
-SerifManager.prototype.left_image = function () {};
-SerifManager.prototype.is_right_talking = function () {};
-SerifManager.prototype.is_left_talking = function () {};
-SerifManager.prototype.font_color = function () {};
-SerifManager.prototype.is_end = function () {};
-SerifManager.prototype.is_background_changed = function () {};
-SerifManager.prototype.background_image = function () {};
+SerifManager.prototype.init = function (script) {
+	return console.error("this method is abolished.");
+};
+SerifManager.prototype.setAutoStart = function (flag) {
+	return console.error("this method is abolished.");
+};
+SerifManager.prototype.isEnd = function () {
+	return console.error("this method is abolished.");
+};
+SerifManager.prototype.isStart = function () {
+	return console.error("this method is abolished.");
+};
+SerifManager.prototype.next = function () {
+	return console.error("this method is abolished.");
+};
+SerifManager.prototype._showBackground = function(script) {
+	return console.error("this method is abolished.");
+};
+SerifManager.prototype._showChara = function(script) {
+	return console.error("this method is abolished.");
+};
+SerifManager.prototype._setOption = function(script) {
+	return console.error("this method is abolished.");
+};
+SerifManager.prototype._printMessage = function (message) {
+	return console.error("this method is abolished.");
+};
+SerifManager.prototype.isWaitingNext = function () {
+	return console.error("this method is abolished.");
+};
+SerifManager.prototype.isEndPrinting = function () {
+	return console.error("this method is abolished.");
+};
+SerifManager.prototype._startPrintMessage = function () {
+	return console.error("this method is abolished.");
+};
+SerifManager.prototype._cancelPrintMessage = function () {
+	return console.error("this method is abolished.");
+};
+SerifManager.prototype.startPrintMessage = function () {
+	return console.error("this method is abolished.");
+};
+SerifManager.prototype.cancelPrintMessage = function () {
+	return console.error("this method is abolished.");
+};
+SerifManager.prototype.isBackgroundChanged = function () {
+	return console.error("this method is abolished.");
+};
+SerifManager.prototype.getBackgroundImageName = function () {
+	return console.error("this method is abolished.");
+};
+SerifManager.prototype.getImageName = function (pos) {
+	return console.error("this method is abolished.");
+};
+SerifManager.prototype.getChara = function (pos) {
+	return console.error("this method is abolished.");
+};
+SerifManager.prototype.isTalking = function (pos) {
+	return console.error("this method is abolished.");
+};
+SerifManager.prototype.getOption = function () {
+	return console.error("this method is abolished.");
+};
+SerifManager.prototype.lines = function () {
+	return console.error("this method is abolished.");
+};
+SerifManager.prototype.getSerifRowsCount = function () {
+	return console.error("this method is abolished.");
+};
+SerifManager.prototype.right_image = function () {
+	return console.error("this method is abolished.");
+};
+SerifManager.prototype.left_image = function () {
+	return console.error("this method is abolished.");
+};
+SerifManager.prototype.is_right_talking = function () {
+	return console.error("this method is abolished.");
+};
+SerifManager.prototype.is_left_talking = function () {
+	return console.error("this method is abolished.");
+};
+SerifManager.prototype.font_color = function () {
+	return console.error("this method is abolished.");
+};
+SerifManager.prototype.is_end = function () {
+	return console.error("this method is abolished.");
+};
+SerifManager.prototype.is_background_changed = function () {
+	return console.error("this method is abolished.");
+};
+SerifManager.prototype.background_image = function () {
+	return console.error("this method is abolished.");
+};
 
 module.exports = SerifManager;
