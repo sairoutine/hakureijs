8132ce9ca53b500f1acef95348975736582b916a
sairoutine <sairoutine@gmail.com>
2018-02-25 03:12:39 +0900

    add: scenario manager test

---
+++ test/manager/scenario.js
@@ -0,0 +1,49 @@
+'use strict';
+var JSDOM = require("jsdom").JSDOM;
+var dom = new JSDOM("<!DOCTYPE html><p>Hello world</p>");
+var canvasMockify = require('canvas-mock');
+
+var assert = require('assert');
+var Core = require('../../core');
+var ScenarioManager = require('../../manager/scenario');
+
+describe('ScenarioManager', function() {
+	var core, scenario;
+    before(function() {
+		// canvas mock
+		var canvas = dom.window.document.createElement("canvas");
+		canvasMockify(canvas);
+
+		// use timeout mock
+		global.setTimeout = function(){};
+		global.clearTimeout = function(){};
+
+		core = new Core(canvas);
+		scenario = new ScenarioManager(core);
+	});
+
+    describe('#init()', function() {
+		var script = [];
+        it('does not occur error', function(done) {
+			scenario.init(script);
+			done();
+		});
+	});
+
+    describe('#start()', function() {
+		var script = [
+			{"chara": "chara1", "serif": "セリフ1"},
+			{"chara": "chara2", "serif": "セリフ2"}
+		];
+
+		beforeEach(function() {
+			scenario.init(script);
+		});
+
+        it('should start at first serif script', function() {
+			scenario.start();
+			assert(scenario.getCurrentCharaNameByPosition() === "chara1");
+		});
+	});
+
+});
