6c86dfdbd8f7aeb7369aaa9a0e9523c94fd6d31a
sairoutine <sairoutine@gmail.com>
2018-02-25 16:38:46 +0900

    implement ui text class

--- object/ui/base.js
+++ object/ui/base.js
@@ -3,22 +3,68 @@
 var BaseObject = require('../base');
 var Util = require('../../util');
 
-var ObjectUIBase = function(core) {
+var ObjectUIBase = function(scene, option) {
 	BaseObject.apply(this, arguments);
+
+	option = option || {};
+
+	this._default_property = {
+		x: option.x || 0,
+		y: option.y || 0,
+	};
+
+	// event handler
+	this._event_to_callback = {
+		beforedraw: function () {},
+	};
+
+	this._show_call_count = 0;
 };
 Util.inherit(ObjectUIBase, BaseObject);
 
 ObjectUIBase.prototype.init = function() {
 	BaseObject.prototype.init.apply(this, arguments);
+
+	this._show_call_count = 0;
+
+	// postion
+	this.x(this._default_property.x);
+	this.y(this._default_property.y);
+
+	// default
+	this.show();
+};
+
+ObjectUIBase.prototype.on = function (event, callback) {
+	this._event_to_callback[event] = callback;
+
+	return this;
+};
+ObjectUIBase.prototype.removeEvent = function (event) {
+	this._event_to_callback[event] = function(){};
+
+	return this;
 };
 
 ObjectUIBase.prototype.beforeDraw = function() {
 	BaseObject.prototype.beforeDraw.apply(this, arguments);
 
+	this._event_to_callback.beforedraw.apply(this);
 };
 
 ObjectUIBase.prototype.draw = function() {
 	BaseObject.prototype.draw.apply(this, arguments);
+};
+
+ObjectUIBase.prototype.isShow = function() {
+	return this._show_call_count > 0;
+};
+
+ObjectUIBase.prototype.show = function() {
+	++this._show_call_count;
+};
+ObjectUIBase.prototype.hide = function() {
+	--this._show_call_count;
 };
 
 module.exports = ObjectUIBase;
--- object/ui/text.js
+++ object/ui/text.js
@@ -2,14 +2,32 @@
 
 var BaseObjectUI = require('./base');
 var Util = require('../../util');
-
-var ObjectUIText = function(core) {
+var ObjectUIText = function(scene, option) {
 	BaseObjectUI.apply(this, arguments);
+
+	option = option || {};
+
+	this._default_property = Util.assign(this._default_property, {
+		text:      option.text || "",
+		textColor: option.textColor || "black",
+		textSize:  option.textSize  || "24px",
+		textAlign: option.textAlign || "left",
+	});
 };
 Util.inherit(ObjectUIText, BaseObjectUI);
 
+Util.defineProperty(ObjectUIText, "text");
+Util.defineProperty(ObjectUIText, "textColor");
+Util.defineProperty(ObjectUIText, "textSize");
+Util.defineProperty(ObjectUIText, "textAlign");
+
 ObjectUIText.prototype.init = function() {
 	BaseObjectUI.prototype.init.apply(this, arguments);
+
+	this.text(this._default_property.text);
+	this.textColor(this._default_property.textColor);
+	this.textSize(this._default_property.textSize);
+	this.textAlign(this._default_property.textAlign);
 };
 
 ObjectUIText.prototype.beforeDraw = function() {
@@ -18,7 +36,18 @@ ObjectUIText.prototype.beforeDraw = function() {
 };
 
 ObjectUIText.prototype.draw = function() {
+	if (!this.isShow()) return;
 	BaseObjectUI.prototype.draw.apply(this, arguments);
+
+	var ctx = this.core.ctx;
+
+	ctx.save();
+	ctx.fillStyle = this.textColor();
+	ctx.textAlign = this.textAlign();
+	ctx.font = this.textSize() + " 'sans-serif'";
+	ctx.fillText(this.text(), this.x(), this.y());
+	ctx.restore();
 };
+
 
 module.exports = ObjectUIText;
