929d9086bfdab350a5e097a56ef53d8b5aaa2049
sairoutine <sairoutine@gmail.com>
2018-03-11 04:10:23 +0900

    add: width and height property to ui image class

--- object/ui/image.js
+++ object/ui/image.js
@@ -10,18 +10,30 @@ var ObjectUIImage = function(scene, option) {
 	this._default_property = Util.assign(this._default_property, {
 		image_name: option.image_name || null,
 		scale:      option.scale      || 1,
+		width:      option.width      || null,
+		height:     option.height     || null,
 	});
 };
 Util.inherit(ObjectUIImage, BaseObjectUI);
 
 Util.defineProperty(ObjectUIImage, "image_name");
 Util.defineProperty(ObjectUIImage, "scale");
+Util.defineProperty(ObjectUIImage, "width");
+Util.defineProperty(ObjectUIImage, "height");
 
 ObjectUIImage.prototype.init = function() {
 	BaseObjectUI.prototype.init.apply(this, arguments);
 
 	this.image_name(this._default_property.image_name);
 	this.scale(this._default_property.scale);
+	this.width(this._default_property.width);
+	this.height(this._default_property.height);
+
+	if (!this.width() && !this.height()) {
+		var image = this.core.image_loader.getImage(this.image_name());
+		this.width(image.width * this.scale());
+		this.height(image.height * this.scale());
+	}
 };
 
 ObjectUIImage.prototype.beforeDraw = function() {
