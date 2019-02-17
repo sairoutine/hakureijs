e99ab6d715caae7add6ece73dc920b2bbbd75988
sairoutine <sairoutine@gmail.com>
2019-02-18 01:02:05 +0900

    add: repeat method for old browser

--- util.js
+++ util.js
@@ -151,6 +151,36 @@ var Util = {
 			return this[private_prop_name];
 		};
 	},
+
+	// for old browser
+	repeat: function(str, count) {
+		// To convert string to integer.
+		count = +count;
+		if (count < 0) {
+		  throw new RangeError('repeat count must be non-negative');
+		}
+		if (count === Infinity) {
+		  throw new RangeError('repeat count must be less than infinity');
+		}
+		count |= 0; // floors and rounds-down it.
+		if (str.length === 0 || count === 0) {
+		  return '';
+		}
+		// Ensuring count is a 31-bit integer allows us to heavily optimize the
+		// main part. But anyway, most current (August 2014) browsers can't handle
+		// strings 1 << 28 chars or longer, so:
+		if (str.length * count >= (1 << 28)) {
+		  throw new RangeError('repeat count must not overflow maximum string size');
+		}
+
+		count >>= 1
+		while (count) { // shift it by multiple of 2 because this is binary summation of series
+		   str += str; // binary summation
+			count >>= 1
+		}
+		str += str.substring(0, str.length * count - str.length);
+		return str;
+	},
 };
 
 module.exports = Util;
