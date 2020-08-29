73502c6b74e5a212ceb8f6cb2fd92b52b1dd32fd
sairoutine <sairoutine@gmail.com>
2020-08-29 23:53:49 +0900

    update: rename image_name -> imageName

--- object/ui/image.js
+++ object/ui/image.js
@@ -8,15 +8,15 @@ var ObjectUIImage = function(scene, option) {
 	option = option || {};
 
 	this._default_property = Util.assign(this._default_property, {
-		image_name: option.image_name || null,
-		scale:      option.scale      || 1,
-		width:      option.width      || null,
-		height:     option.height     || null,
+		imageName: option.imageName  || null,
+		scale:     option.scale      || 1,
+		width:     option.width      || null,
+		height:    option.height     || null,
 	});
 };
 Util.inherit(ObjectUIImage, BaseObjectUI);
 
-Util.defineProperty(ObjectUIImage, "image_name");
+Util.defineProperty(ObjectUIImage, "imageName");
 Util.defineProperty(ObjectUIImage, "scale");
 Util.defineProperty(ObjectUIImage, "width");
 Util.defineProperty(ObjectUIImage, "height");
@@ -24,13 +24,13 @@ Util.defineProperty(ObjectUIImage, "height");
 ObjectUIImage.prototype.init = function() {
 	BaseObjectUI.prototype.init.apply(this, arguments);
 
-	this.image_name(this._default_property.image_name);
+	this.imageName(this._default_property.imageName);
 	this.scale(this._default_property.scale);
 	this.width(this._default_property.width);
 	this.height(this._default_property.height);
 
 	if (!this.width() && !this.height()) {
-		var image = this.core.image_loader.getImage(this.image_name());
+		var image = this.core.image_loader.getImage(this.imageName());
 		this.width(image.width * this.scale());
 		this.height(image.height * this.scale());
 	}
@@ -44,7 +44,7 @@ ObjectUIImage.prototype.update = function() {
 ObjectUIImage.prototype.draw = function() {
 	if (!this.isShow()) return;
 
-	var image = this.core.image_loader.getImage(this.image_name());
+	var image = this.core.image_loader.getImage(this.imageName());
 	var width  = image.width  * this.scale();
 	var height = image.height * this.scale();
 	var ctx = this.core.ctx;
